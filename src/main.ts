c928cd62fef0daafcd82d3c16d1ac59150cb0ac8
Emerson <emerson.matosokl10@gmail.com>
2025-04-14 18:02:27 -0300

    feat: ajustar importações e organização do código no main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-// Vuetify
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' // Ícones
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
   components,
