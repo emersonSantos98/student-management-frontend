f4f2cd3dd5ee912f93f47cd27aa59fb852d2847f
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 22:05:30 -0300

    feat: adicionar visualização de lista de estudantes

---
+++ src/views/student/StudentListView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Student List</h1>
+    <p>This is the student list view.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
