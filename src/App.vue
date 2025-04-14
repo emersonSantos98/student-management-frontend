bb627c16e17e1262f8f5301c5a31a4a77271034c
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 23:54:46 -0300

    feat: adicionar lógica para exibir cabeçalho e rodapé condicionalmente com base na autenticação

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,49 @@
 <template>
   <v-app>
-    <v-layout class="rounded rounded-md">
-      <AppHeader
-        :isAuthenticated="isAuthenticated"
-        @logout="logout"
-      />
+    <template v-if="!isLoginPage">
+      <v-layout>
+        <AppNavigation
+          v-if="isAuthenticated"
+          @logout="logout"
+        />
 
-      <AppNavigation
-        v-if="isAuthenticated"
-        @logout="logout"
-      />
+        <AppHeader
+          :isAuthenticated="isAuthenticated"
+          @logout="logout"
+        />
 
-      <v-main>
-        <v-container fluid>
-          <router-view />
-        </v-container>
-      </v-main>
-    </v-layout>
+        <v-main>
+          <v-container fluid>
+            <router-view />
+          </v-container>
+        </v-main>
 
-    <AppFooter />
+        <AppFooter />
+      </v-layout>
+    </template>
+
+    <template v-else>
+      <router-view />
+    </template>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
 import AppNavigation from '@/components/AppNavigation.vue'
 import AppFooter from '@/components/AppFooter.vue'
 
 const router = useRouter()
+const route = useRoute()
 
 const isAuthenticated = computed(() => {
   return !!localStorage.getItem('token')
+})
+
+const isLoginPage = computed(() => {
+  return route.path === '/login'
 })
 
 const logout = () => {
