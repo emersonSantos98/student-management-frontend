9e687e64b372a3864f069061fa0718879b385e7b
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 23:33:40 -0300

    feat: adicionar rodapé ao aplicativo com o ano atual

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer app class="bg-surface-variant">
+    <div class="text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>+A Educação</strong>
+    </div>
+  </v-footer>
+</template>
