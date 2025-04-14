fe3f4006c566fa6a0fa5d8e8fe65f94b6f8dd570
Emerson <emerson.matosokl10@gmail.com>
2025-04-14 01:22:24 -0300

    feat: adicionar serviço de curso para gerenciar grupos de cursos

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const router = createRouter({
       component: () => import('@/views/student/StudentListView.vue'),
       meta: { requiresAuth: true }
     },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: () => import('@/views/course-groups/CourseGroupsView.vue'),
+      meta: { requiresAuth: true }
+    },
   ]
 })
 
