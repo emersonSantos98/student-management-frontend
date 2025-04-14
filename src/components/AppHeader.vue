c42f7276ea89527ea5b7506dec32c4b7e69cce20
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 23:33:34 -0300

    feat: adicionar cabeçalho de aplicativo com funcionalidade de logout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar color="primary">
+    <v-app-bar-title>Sistema de Gerenciamento de Alunos</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn v-if="isAuthenticated" icon @click="logout">
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isAuthenticated: boolean
+}>()
+
+const emit = defineEmits(['logout'])
+
+const logout = () => {
+  emit('logout')
+}
+</script>
