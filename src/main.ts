3652d122b56a19aafbb6188840c4ba5d28bbde4e
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 22:02:54 -0300

    feat: adicionar autenticação nas rotas e configurar Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ícones
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light'
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/auth/LoginView.vue.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/students',
+      name: 'students',
+      component: () => import('@/views/student/StudentListView.vue'),
+      meta: { requiresAuth: true }
     },
-  ],
+  ]
+})
+
+// Guarda de navegação para autenticação
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token') // Ajuste conforme sua lógica de autenticação
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
